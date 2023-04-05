ec2c9e16a77495ea772b9f19426791e1754a8a50
alexcrichton <alex@alexcrichton.com>
2023-04-05 17:46:12 +0000

    Deploying to gh-pages from @ bytecodealliance/wasmtime@bf741955f0e5cd248d53f6ea7d76127624b52d1b 🚀

--- api/implementors/wasmtime/component/func/typed/trait.Lower.js
+++ api/implementors/wasmtime/component/func/typed/trait.Lower.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
-"component_test_util":[["impl Lower for <a class=\"struct\" href=\"component_test_util/struct.Float32.html\" title=\"struct component_test_util::Float32\">Float32</a>"],["impl Lower for <a class=\"struct\" href=\"component_test_util/struct.Float64.html\" title=\"struct component_test_util::Float64\">Float64</a>"]],
+"component_test_util":[["impl Lower for <a class=\"struct\" href=\"component_test_util/struct.Float64.html\" title=\"struct component_test_util::Float64\">Float64</a>"],["impl Lower for <a class=\"struct\" href=\"component_test_util/struct.Float32.html\" title=\"struct component_test_util::Float32\">Float32</a>"]],
 "wasmtime":[],
 "wiggle":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
